e6ba2149439f60f9f62728483776a0bca0dc35c8
Wojcik <dwojcik@luxoft.com>
2025-06-10 10:35:42 +0200

    wip

---
+++ rules/actions.ts
@@ -0,0 +1,40 @@
+import { calculateSpreadForCurve } from "./calculate";
+
+const handleConvert = (
+  context: Ticket,
+  update: { path: string; value: any; applyToTarget?: boolean } | undefined
+): Partial<{ rate0: number; rate1: number; spread: number }> | undefined => {
+  if (!update?.value) return;
+
+  const shouldRecalculate =
+    update.path.includes("maturityDate") ||
+    (update.path.includes("spread") && update.applyToTarget);
+
+  if (shouldRecalculate) {
+    return calculateSpreadForCurve(context, update);
+  }
+
+  return;
+};
+
+export const actions = [
+  {
+    fieldUpdate: {
+      "legs.0.rate": {
+        type: "convert",
+        convert: (_field, _input, context: Ticket, update) =>
+          handleConvert(context, update)
+      },
+      "legs.1.rate": {
+        type: "convert",
+        convert: (_field, _input, context: Ticket, update) =>
+          handleConvert(context, update)
+      },
+      "spread": {
+        type: "convert",
+        convert: (_field, _input, context: Ticket, update) =>
+          handleConvert(context, update)
+      }
+    }
+  }
+];
