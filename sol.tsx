036a5a8c21ad5c65c5b1448dd013604174efa1b6
Wojcik <dwojcik@luxoft.com>
2025-07-10 11:23:30 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,40 +1,14 @@
-const stompUpdatedViewIdsRef = useRef<Set<string>>(new Set());
+if (versionGreaterOrEqualThan(contentVersion, '1.20.1')) {
+  iterateViews(content, ({ grid }) => {
+    const { columnState } = grid.gridState;
 
-const onMessage = async (incomingView: IncomingView) => {
-  const incomingSenderId = incomingView.content?.senderId;
+    if (!Array.isArray(columnState)) return;
 
-  if (incomingSenderId === senderId) {
-    return;
-  }
+    const index = columnState.findIndex(col => col.colId === 'bbgActions');
+    if (index === -1) return;
 
-  await modifyState?.(async (draft) => {
-    const existingView = getItemById(draft.views, id);
-    if (existingView) {
-      const changed = isExtraChanged?.(existingView.extra, incomingView.content?.extra);
-      if (changed) {
-        existingView.extra = incomingView.content?.extra;
-        stompUpdatedViewIdsRef.current.add(id);
-      }
-    }
+    const [bbgActionsCol] = columnState.splice(index, 1);
+    columnState.splice(1, 0, bbgActionsCol); // indeks 1 = druga kolumna
   });
-};
-
-const updateSharedViews = async () => {
-  const sharedViewsIds = updateSharedViews.map((view) => view.id);
-
-  for (const id of sharedViewsIds) {
-    const prevView = prevSharedViews.find((v) => v.id === id);
-    const nextView = sharedViews.find((v) => v.id === id);
-    const changed = prevView && nextView && isExtraChanged?.(prevView.extra, nextView.extra);
-
-    // Skip if updated by STOMP
-    if (stompUpdatedViewIdsRef.current.has(id)) {
-      stompUpdatedViewIdsRef.current.delete(id);
-      continue;
-    }
-
-    if (changed) {
-      await shareItem(id);
-    }
-  }
-};
+  return;
+}
