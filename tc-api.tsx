38e0229312fedc98c36fa3f2ca3ec308d0a6e4e2
Wojcik <dwojcik@luxoft.com>
2025-07-22 09:49:10 +0200

    wip

--- tc-api.tsx
+++ tc-api.tsx
@@ -1,9 +1,1 @@
-const wrapWithProviders = (
-  providers: extension[] | undefined,
-  children: JSX.Element | JSX.Element[]
-): JSX.Element => {
-  return (providers ?? []).reduceRight((acc, extension, index) => {
-    const Provider = extension.ContextProvider;
-    return <Provider key={index}>{acc}</Provider>;
-  }, children);
-};
+React.FC<{ children: React.ReactNode }>
