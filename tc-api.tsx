802105352360fa8a1fbb3683da20350514b160ec
Wojcik <dwojcik@luxoft.com>
2025-07-23 08:59:49 +0200

    wip

--- tc-api.tsx
+++ tc-api.tsx
@@ -1,1 +1,29 @@
-  ContextProvider: <T = {}>(props: T & { children?: React.ReactNode }) => JSX.Element
+function createAction(): Action {
+  const base = {
+    id,
+    label: stateLabel,
+    description: stateDescription,
+    active: false
+  };
+
+  if (isFunction) {
+    return {
+      ...base,
+      mnemonic: stateMnemonic,
+      inTab: stateInTab,
+      tabName: stateTabName,
+      panel: statePanel
+    } satisfies FunctionAction;
+  }
+
+  if (isLaunchpad) {
+    return {
+      ...base,
+      group: stateGroup
+    } satisfies LaunchpadAction;
+  }
+
+  return base;
+}
+
+const newAction = createAction();
