f992def6dd44d3254fd543ab10a9f9e0fc8748dd
Wojcik <dwojcik@luxoft.com>
2025-07-23 16:59:16 +0200

    wip

--- tc-api.tsx
+++ tc-api.tsx
@@ -1,25 +1,7 @@
-
-    let newAction: Action;
-
-    if (isFunction) {
-      newAction = {
-        id,
-        label: stateLabel,
-        description: stateDescription,
-        active: false,
-        mnemonic: stateMnemonic,
-        inTab: stateInTab,
-        tabName: stateTabName,
-        panel: statePanel,
-      } as FunctionAction;
-    } else if (isLaunchpad) {
-      newAction = {
-        id,
-        label: stateLabel,
-        description: stateDescription,
-        active: false,
-        group: stateGroup,
-      } as LaunchpadAction;
-    } else {
-      throw new Error("Must select either Launchpad or Function action type");
-    }
+const setGroupingMode = useCallback((groupingMode: boolean) => {
+  if (!fnRef.current) {
+    console.warn('updateSelectedView is not yet available');
+    return;
+  }
+  fnRef.current(groupingMode);
+}, []);
