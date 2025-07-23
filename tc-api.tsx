c4c07f3ff2132b52d7e95d23b086096f3431b229
Wojcik <dwojcik@luxoft.com>
2025-07-23 09:03:33 +0200

    wip

--- tc-api.tsx
+++ tc-api.tsx
@@ -1,41 +1,12 @@
-// 🔹 Type Definitions
-interface BaseAction {
-  id: string;
-  label: string;
-  description: string;
-  active: boolean;
-}
 
-interface LaunchpadAction extends BaseAction {
-  group: string;
-}
-
-interface FunctionAction extends BaseAction {
-  mnemonic: string;
-  inTab: boolean;
-  tabName: string;
-  panel: string;
-}
-
-type Action = BaseAction | LaunchpadAction | FunctionAction;
-
- const handleSave = () => {
-    const id = crypto.randomUUID();
-
-    // 🔹 Build base action
-    const base: BaseAction = {
-      id,
-      label: stateLabel,
-      description: stateDescription,
-      active: false,
-    };
-
-    // 🔹 Dynamically build and assert type
     let newAction: Action;
 
     if (isFunction) {
       newAction = {
-        ...base,
+        id,
+        label: stateLabel,
+        description: stateDescription,
+        active: false,
         mnemonic: stateMnemonic,
         inTab: stateInTab,
         tabName: stateTabName,
@@ -43,9 +14,12 @@ type Action = BaseAction | LaunchpadAction | FunctionAction;
       } as FunctionAction;
     } else if (isLaunchpad) {
       newAction = {
-        ...base,
+        id,
+        label: stateLabel,
+        description: stateDescription,
+        active: false,
         group: stateGroup,
       } as LaunchpadAction;
     } else {
-      newAction = base;
+      throw new Error("Must select either Launchpad or Function action type");
     }
