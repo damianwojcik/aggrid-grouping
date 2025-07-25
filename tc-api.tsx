810cff33b78130644e36e37031f794492f8350fa
Wojcik <dwojcik@luxoft.com>
2025-07-25 11:56:08 +0200

    wip

--- tc-api.tsx
+++ tc-api.tsx
@@ -1,7 +1,22 @@
-const setGroupingMode = useCallback((groupingMode: boolean) => {
-  if (!fnRef.current) {
-    console.warn('updateSelectedView is not yet available');
-    return;
-  }
-  fnRef.current(groupingMode);
-}, []);
+export const FrameworkContextProvider = ({extensions, children}) => {
+
+const extensionsProviders = extensions.map(extension => extension.ContextProvider);
+return(
+  <AppContextProvider>
+    <StorageContextProvider>
+      {/* modify extensions now to be extensionsProivders from ln3, modify wrapWithProviders fn */}
+      {wrapWithProviders(extensions, children)}
+    </StorageContextProvider>
+  </AppContextProvider>
+)
+
+}
+
+const wrapWithProviders = (
+  providers: React.ComponentType<{ children: React.ReactNode }>[],
+  children: JSX.Element | JSX.Element[]
+) => {
+  return providers.reduceRight((acc, Provider, index) => {
+    return <Provider key={index}>{acc}</Provider>;
+  }, children as JSX.Element);
+};
