a24d96b44a58738e8f5c0ef30013420c7f733ae8
Wojcik <dwojcik@luxoft.com>
2025-08-01 15:46:57 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,8 +1,6 @@
-// All params of updateTemporaryView
-type UpdateTemporaryViewParams = Parameters<ViewApi['updateTemporaryView']>;
-
-// Remove first param (create)
-type UpdateTemporaryViewWithoutCreateParams = UpdateTemporaryViewParams extends [any, ...infer Rest] ? Rest : never;
-
-// The function type without the first param:
-type UpdateTemporaryViewWithoutCreate = (...args: UpdateTemporaryViewWithoutCreateParams) => ReturnType<ViewApi['updateTemporaryView']>;
+const expandAllChildren = (node: RowNode): void => {
+  node.childrenAfterGroup?.forEach((child) => {
+    child.setExpanded(true);
+    expandAllChildren(child);
+  });
+};
