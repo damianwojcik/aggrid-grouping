33a697401997ecfe14a7d05445b80f364a91ba80
Wojcik <dwojcik@luxoft.com>
2025-08-05 11:02:32 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,10 +1,36 @@
-const toggleAllChildren = (node: RowNode): void => {
-  const shouldExpand = !node.expanded;
+function getGroupingData(node: any): { field: string; value: any }[] {
+  const result: { field: string; value: any }[] = [];
 
-  const toggle = (n: RowNode) => {
-    n.setExpanded(shouldExpand);
-    n.childrenAfterGroup?.forEach(toggle);
-  };
+  let currentNode = node;
+  while (currentNode) {
+    if (currentNode.field) {
+      result.push({
+        field: currentNode.field,
+        value: currentNode.data?.[currentNode.field],
+      });
+    }
+    currentNode = currentNode.parent;
+  }
 
-  toggle(node);
-};
+  // Reverse so that the top-most parent is first
+  return result.reverse();
+}
+
+{
+  name: 'View Data',
+  action: () => {
+    const MY_DATA = getGroupingData(node);
+
+    const groupingMatchers = MY_DATA.map((item) =>
+      createMatcher({
+        operator: 'and',
+        comparison: groupingComparisonOperator,
+        field: item.field,
+        value: item.value,
+        test: String(item.value),
+      })
+    );
+
+    console.log(groupingMatchers);
+  }
+}
