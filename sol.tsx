36fb8c3368a34cdf2fd7ea621b5565cfa83e48a2
Wojcik <dwojcik@luxoft.com>
2025-08-06 07:08:59 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,25 +1,2 @@
-function getGroupingData(node: IRowNode<any>): { field: string; value: string }[] {
-  const result: { field: string; value: string }[] = [];
-
-  let currentNode: IRowNode<any> | null = node;
-
-  while (currentNode) {
-    const { field, data } = currentNode;
-
-    if (field && data) {
-      const rawValue = data[field];
-
-      // Only include if value is not null, undefined, or empty string
-      if (rawValue !== undefined && rawValue !== null && rawValue !== '') {
-        result.push({
-          field,
-          value: String(rawValue),
-        });
-      }
-    }
-
-    currentNode = currentNode.parent ?? null;
-  }
-
-  return result.reverse();
-}
+const existingView = id ? getItemById(views, id) : undefined;
+const draftView = existingView ?? create(ViewType.ViewTemporary, label, id, parentId);
