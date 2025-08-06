9d5a7350e14144b4e5dde9cf9dfc41bb34c3f6c5
Wojcik <dwojcik@luxoft.com>
2025-08-06 08:23:14 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,11 +1,3 @@
-const draftViewId = draftView.path.at(-1);
-
-const updatedTemporaryViews = temporaryViews.some(
-  v => v.path.at(-1) === draftViewId
-)
-  ? temporaryViews.map(v =>
-      v.path.at(-1) === draftViewId ? draftView : v
-    )
-  : [...temporaryViews, draftView];
-
-store.set(temporaryViewsAtom, updatedTemporaryViews);
+const views = useMemo(() => {
+  return [...storageViews, ...temporaryViews].map(view => ({ ...view }));
+}, [storageViews, temporaryViews]);
