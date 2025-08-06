2407e0e1540011bc5d6835ad1438da4aa56560a8
Wojcik <dwojcik@luxoft.com>
2025-08-06 09:24:36 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,3 +1,9 @@
-const views = useMemo(() => {
-  return [...storageViews, ...temporaryViews].map(view => ({ ...view }));
-}, [storageViews, temporaryViews]);
+function hasGrouping(
+  view: any
+): view is { extra: { grouping: { enabled: boolean } } } {
+  return (
+    'extra' in view &&
+    'grouping' in view.extra &&
+    typeof view.extra.grouping?.enabled === 'boolean'
+  );
+}
