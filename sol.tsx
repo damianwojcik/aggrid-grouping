68425e38d58aa9412d020d913a68aaf4e9a1d5a7
Wojcik <dwojcik@luxoft.com>
2025-08-11 10:17:43 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,2 +1,45 @@
-const targetIndex = arr.findIndex(v => v.path.length === 1 && v.path[0] === 'abc');
-arr.splice(targetIndex + 1, 0, newView);
+// whatever gives you the current view type
+const viewType = currentView?.type; // 'view-temporary' | 'view-normal' | ...
+
+return (
+  <div className="app" data-view-type={viewType}>
+    <Toolbar>
+      <button className="btn-toggle">Toggle</button>
+      <button className="btn-apply-all">Apply to all views</button>
+      {/* ...rest */}
+    </Toolbar>
+    {/* rest of app */}
+  </div>
+);
+
+
+/* Hide entirely when view is temporary */
+[data-view-type="view-temporary"] .btn-toggle,
+[data-view-type="view-temporary"] .btn-apply-all {
+  display: none;
+}
+
+/* If you prefer to keep layout space but disable interaction: */
+/*
+[data-view-type="view-temporary"] .btn-toggle,
+[data-view-type="view-temporary"] .btn-apply-all {
+  visibility: hidden;
+  pointer-events: none;
+}
+*/
+
+
+<div className={s.app} data-view-type={viewType}>
+  <button className={s.toggleBtn}>Toggle</button>
+  <button className={s.applyAllBtn}>Apply to all views</button>
+</div>
+
+
+/* App.module.css */
+[data-view-type="view-temporary"] .toggleBtn,
+[data-view-type="view-temporary"] .applyAllBtn {
+  display: none;
+}
+
+
+document.querySelector('[data-view-type="view-temporary"]')
