f96530654ab16a05268e1b8024df7b202070cfc8
Wojcik <dwojcik@luxoft.com>
2025-08-22 13:35:18 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,5 +1,21 @@
-const sortDefaultFirst = (views: View[]) =>
-  views.sort((a, b) => Number(isDefaultViewDef(b)) - Number(isDefaultViewDef(a)));
-
-const remainingViews = allViews.filter(view => !isTemporaryViewDef(view));
-sortDefaultFirst(remainingViews);
+/**
+ * Creates or updates a temporary view inside the `viewsComponent`.
+ *
+ * - If `id` is provided and matches a temporary view, it updates that view.
+ * - If no matching view is found and `selectedView?.type !== ViewType.ViewTemporary`, a new temporary view is created.
+ * - The provided `update` callback can mutate the `draftView` before saving.
+ * - Automatically sets `label`, `parentId`, `path`, and clones `extra` from `selectedView` when appropriate.
+ * - Disables grouping if the view has grouping enabled.
+ *
+ * @param {Object} viewInfo - Info about the view to create or update.
+ * @param {string} [viewInfo.id] - ID of the view to update. If not provided, a new ID will be generated.
+ * @param {string} [viewInfo.label] - Label of the view.
+ * @param {string} [viewInfo.parentId] - Optional parent view ID to define the view's path.
+ * @param {ViewType} [viewInfo.type=ViewType.ViewTemporary] - Type of the view to create (defaults to `ViewTemporary`).
+ *
+ * @param {(draftView: StrictViewContent) => Promise<void>} [update] - Optional async callback to modify the draft view before saving.
+ *
+ * @returns {Promise<string>} Resolves with the ID of the created or updated view.
+ *
+ * @throws Will reject the promise if the view cannot be found or created.
+ */
