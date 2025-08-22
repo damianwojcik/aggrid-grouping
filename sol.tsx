4d4fe6c452998a714a22c558df3869fef6611e79
Wojcik <dwojcik@luxoft.com>
2025-08-22 08:32:41 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,26 +1,12 @@
 const deserialize = (content: ViewsState, removedContent: ViewsState) => {
-  const currentIds = new Set(
-    content.viewsComponent.views.map((v) => getItemId(v))
-  );
+  const existingIds = new Set(content.viewsComponent.views.map(view => getItemId(view)));
 
-  const strippedIds = new Set(
-    removedContent.viewsComponent.views.map((v) => getItemId(v))
-  );
-
-  // final result: przywróć tylko te, które:
-  // - są w removedContent
-  // - nie są już w stanie (czyli nie zostały usunięte w UI)
-  const toRestore = removedContent.viewsComponent.views.filter(
-    (v) => {
-      const id = getItemId(v);
-      return !currentIds.has(id) && strippedIds.has(id);
-    }
+  const viewsToRestore = removedContent.viewsComponent.views.filter(
+    view => !existingIds.has(getItemId(view))
   );
 
   content.viewsComponent.views = [
     ...content.viewsComponent.views,
-    ...toRestore
-  ].filter(
-    (v, i, arr) => i === arr.findIndex((x) => getItemId(x) === getItemId(v))
-  );
+    ...viewsToRestore
+  ];
 };
