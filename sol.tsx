6696a7c12a1e0ceeb02cd69d044a240bffa025fb
Wojcik <dwojcik@luxoft.com>
2025-08-22 07:07:35 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,44 +1,4 @@
-const updateTemporaryView: UpdateTemporaryView = async (
-  { id, label, parentId, type = ViewType.ViewTemporary },
-  update
-) => {
-  const ensuredId = id ?? crypto.randomUUID();
-
-  return new Promise<string>((resolve) => {
-    updateViewsComponent((viewsComponentDraft) => {
-      const temporaryViews = viewsComponentDraft.views.filter(isTemporaryViewDef);
-      const isDefaultView = type === ViewType.ViewDefault;
-
-      const existingView = id
-        ? getItemById(temporaryViews, id)
-        : isDefaultView
-        ? undefined
-        : selectedView
-        ? findParentViewBtItemId(temporaryViews, getItemId(selectedView))
-        : undefined;
-
-      const shouldCreateNew = !existingView;
-
-      const draftView = shouldCreateNew
-        ? createDefaultStrictView(
-            type,
-            label ?? TEMPORARY_VIEW_LABEL,
-            ensuredId,
-            parentId
-          )
-        : { ...existingView, ...update, label, parentId, type };
-
-      if (shouldCreateNew) {
-        viewsComponentDraft.views.unshift(draftView);
-      } else {
-        const idx = viewsComponentDraft.views.findIndex(
-          (v) => getItemId(v) === getItemId(existingView)
-        );
-        if (idx !== -1) viewsComponentDraft.views[idx] = draftView;
-      }
-
-      // resolve right here
-      resolve(getItemId(draftView) ?? ensuredId);
-    });
-  });
-};
+deserialize(content, strippedContent); // tymczasowe wracają
+const { strippedContent: newStripped } = serialize(content); // znowu usuwasz
+setStrippedContent(newStripped); // stan jest aktualny
+save(content); // zapis bez tymczasowych
