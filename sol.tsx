3fc6f8eb44e9c845aec4aa40f303ff6e34077d35
Wojcik <dwojcik@luxoft.com>
2025-09-19 12:12:17 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,13 +1,27 @@
-export const ExtensionsContextProvider = ({
-  children,
-  extensions,
-  extensionProps,
-}: ProviderProps) => {
-  const contextValue = useMemo(() => ({ extensions }), [extensions]);
+const getContextMenuItems = hooks.useDynamicCallback(
+  (params: community.GetContextMenuItemsParams): ReturnType<community.GetContextMenuItems> => {
+    const node = params.node;
+    const data: BackendRow = node?.data;
+    const ticketId = data?.[Field.__TicketId] ?? '';
+    const copyTicketId = [copyValue('Copy ticket id', ticketId, node)];
 
-  return (
-    <ExtensionsContext.Provider value={contextValue}>
-      {children}
-    </ExtensionsContext.Provider>
-  );
-};
+    const draft = params.menuItems as (string | community.MenuItemDef)[];
+
+    // Find the first "Copy" section
+    const copyIdx = draft.findIndex(
+      (it) => typeof it !== 'string' && (it as community.MenuItemDef).sectionType === community.SectionType.Copy
+    );
+
+    // Insert our item before that section (or append if not found)
+    if (copyIdx >= 0) {
+      return [
+        ...draft.slice(0, copyIdx),
+        ...copyTicketId,
+        ...draft.slice(copyIdx),
+      ];
+    }
+    return [...draft, ...copyTicketId];
+  }
+);
+
+return { getContextMenuItems };
