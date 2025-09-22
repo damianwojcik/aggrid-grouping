4759d76f0dc0747e784135c4e9b5ae79aff1cff8
Wojcik <dwojcik@luxoft.com>
2025-09-22 11:20:16 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,31 +1,3 @@
-#writeURL(value: ContextItemValue, url: URL) {
-  if (!this.#urlOptions) {
-    return;
-  }
-
-  for (const searchParam of this.#urlOptions.searchParams) {
-    if (value === null) {
-      url.searchParams.delete(searchParam);
-    } else {
-      url.searchParams.delete(searchParam);
-
-      const qs = url.searchParams.toString();
-
-      // encode, but restore commas and turn spaces into '+'
-      const encoded = encodeURIComponent(String(value))
-        .replace(/%2C/gi, ',')
-        .replace(/%20/gi, '+');
-
-      const raw = `${searchParam}=${encoded}`;
-
-      url = new URL(
-        `${url.origin}${url.pathname}` + (qs ? `?${qs}&${raw}` : `?${raw}`)
-      );
-    }
-  }
-
-  const nextURL = url.href;
-  if (nextURL !== location.href) {
-    ContextItem.#historyReplaceState.call(history, {}, "", url.href);
-  }
-}
+const encodeValueReadable(v: string) => encodeURIComponent(v)
+    .replace(/%2C/gi, ',')  // keep commas
+    .replace(/%20/gi, '+'); // show spaces as +
