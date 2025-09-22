ab149d616000b069054ed95c3266ac4be667bba7
Wojcik <dwojcik@luxoft.com>
2025-09-22 11:05:01 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,11 +1,5 @@
-const insertBeforeSection = (
-  items: MenuItem[],
-  section: ContextMenuItemsSection,
-  newItem: MenuItem
-): MenuItem[] => {
-  const idx = items.findIndex(
-    it => typeof it !== "string" && it.contextMenuItemSection === section
-  );
-  if (idx === -1) return [...items, newItem]; // fallback: append if no match
-  return [...items.slice(0, idx), newItem, ...items.slice(idx)];
-};
+url.searchParams.set(searchParam, value);
+
+url = new URL(
+  `${url.origin}${url.pathname}?${url.searchParams.toString()}&${searchParam}=${value}`
+);
