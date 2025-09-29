26596079cd358c933ed31fb8b1e354be9640540d
Wojcik <dwojcik@luxoft.com>
2025-09-29 10:53:54 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,12 +1,1 @@
-Hi [Manager’s Name],
-
-I found out on Friday that I will need to undergo cervical spine surgery, and this morning (Monday) the hospital confirmed that I should report on Thursday, October 2nd for the procedure.
-
-Following the surgery, my doctors expect that I will be on medical leave for about 4–6 weeks. I will provide the official sick leave documentation once I receive it.
-
-Could you please advise me on who I should hand over my current tasks to before Thursday? I want to make sure everything is properly covered.
-
-Thank you for your understanding and support.
-
-Best regards,
-Damian
+asfas
