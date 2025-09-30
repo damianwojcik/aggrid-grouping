c95043533e8449cc9c1b8d5e533907732a62da39
Wojcik <dwojcik@luxoft.com>
2025-09-30 10:59:37 +0200

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,1 +1,22 @@
-asfas
+const onceRowDataUpdated = (api: GridApi): Promise<void> =>
+  new Promise(resolve => {
+    const handler = () => {
+      api.removeEventListener('rowDataUpdated', handler);
+      resolve();
+    };
+    api.addEventListener('rowDataUpdated', handler);
+  });
+
+
+  const onSaveClick = async () => {
+  const id = itemId;
+  await modifyState(...);
+
+  await onceRowDataUpdated(api);
+
+  const node = api.getRowNode(id);
+  if (node) {
+    api.ensureIndexVisible(node.rowIndex!);
+    api.startEditingCell({ rowIndex: node.rowIndex!, colKey: 'label' });
+  }
+};
