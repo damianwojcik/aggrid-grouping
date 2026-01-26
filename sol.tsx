15fd4f0564eb21e92b96a1db695d11824dd126a0
Wojcik <dwojcik@luxoft.com>
2026-01-26 14:14:01 +0100

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,40 +1,77 @@
 import { z } from '@ubs.fi.repackaged/zod-4';
+import type { UnknownRecord } from '@ubs.fi.lib/type-utils';
+import { CachePolicy, type StorageOptions } from '@ubs.fi/storage';
+import { viewExtraSchema } from './extraSchema';
+import {
+  createViewsComponentSchema,
+  initialViewsComponentContent,
+} from './viewsComponentSchema';
 
-/* base */
+/* ---------------------------- helpers ---------------------------- */
 
-export const viewExtraSchema = z.object({}).default({});
+type UnwrapDefault<T extends z.ZodTypeAny> =
+  T extends z.ZodDefault<infer U> ? U : T;
 
-/* helpers */
-
-type ResolveViewExtraSchema<T> =
-  T extends { customViewExtraSchema: (base: typeof viewExtraSchema) => infer R }
-    ? R
+type GetContentWithCustomViewExtra<T> =
+  T extends { customViewExtraSchema(param: any): infer R }
+    ? R extends z.ZodTypeAny
+      ? R
+      : typeof viewExtraSchema
     : typeof viewExtraSchema;
 
-/* factories */
-
-export const createViewsComponentSchema = <
-  TExtra extends z.ZodType<unknown>
->(
-  viewExtraSchema: TExtra
-) =>
-  z.object({
-    extra: viewExtraSchema,
-  });
+/* ---------------------------- schema ----------------------------- */
 
 export const createContentSchema = <
-  T extends {
-    customViewExtraSchema?: <S extends typeof viewExtraSchema>(
-      base: S
-    ) => z.ZodType<unknown>;
-  }
->(
-  options: T
-) => {
-  const resolvedViewExtraSchema =
-    options.customViewExtraSchema?.(viewExtraSchema) ?? viewExtraSchema;
-
-  return z.object({
-    viewsComponent: createViewsComponentSchema(resolvedViewExtraSchema),
+  T extends Pick<
+    CreateStorageModelOptions<z.ZodTypeAny>,
+    'customViewExtraSchema'
+  >
+>({
+  customViewExtraSchema,
+}: T) =>
+  z.object({
+    version: z.string(),
+    settings: z.object({}),
+    viewsComponent: createViewsComponentSchema(
+      (customViewExtraSchema?.(viewExtraSchema) ??
+        viewExtraSchema) as GetContentWithCustomViewExtra<T>
+    ),
   });
+
+/* ----------------------------- model ----------------------------- */
+
+export interface CreateStorageModelOptions<
+  T extends z.ZodTypeAny,
+> {
+  createStorageOptions: <Content extends UnknownRecord>(
+    guid: string,
+  ) => Pick<
+    StorageOptions<Content, Content, Content, Content, Content>,
+    'storage'
+  >;
+
+  customViewExtraSchema?: (
+    baseViewExtraSchema: typeof viewExtraSchema,
+  ) => T;
+}
+
+export const createStorageModel = <
+  T extends CreateStorageModelOptions<z.ZodTypeAny>,
+>(
+  options: T,
+) => {
+  const schema = createContentSchema<T>(options);
+
+  type Content = z.output<typeof schema>;
+
+  const initialContent: Content = {
+    version: '0.0.1',
+    settings: {},
+    viewsComponent: initialViewsComponentContent,
+  };
+
+  return {
+    schema,
+    initialContent,
+  };
 };
