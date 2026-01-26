5b7a9f23f15bef06104fe4e9273deed85ea797a4
Wojcik <dwojcik@luxoft.com>
2026-01-26 13:47:42 +0100

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,19 +1,40 @@
+import { z } from '@ubs.fi.repackaged/zod-4';
+
+/* base */
+
+export const viewExtraSchema = z.object({}).default({});
+
+/* helpers */
+
 type ResolveViewExtraSchema<T> =
   T extends { customViewExtraSchema: (base: typeof viewExtraSchema) => infer R }
     ? R
     : typeof viewExtraSchema;
 
-export const createContentSchema = <
-  T extends Pick<CreateStorageModelOptions<z.ZodTypeAny>, 'customViewExtraSchema'>
->({
-  customViewExtraSchema,
-}: T) =>
+/* factories */
+
+export const createViewsComponentSchema = <
+  TExtra extends z.ZodType<unknown>
+>(
+  viewExtraSchema: TExtra
+) =>
   z.object({
-    version: z.string(),
-    settings: z.object({}),
-    viewsComponent: createViewsComponentSchema(
-      (customViewExtraSchema
-        ? customViewExtraSchema(viewExtraSchema)
-        : viewExtraSchema) as ResolveViewExtraSchema<T>
-    ),
+    extra: viewExtraSchema,
   });
+
+export const createContentSchema = <
+  T extends {
+    customViewExtraSchema?: <S extends typeof viewExtraSchema>(
+      base: S
+    ) => z.ZodType<unknown>;
+  }
+>(
+  options: T
+) => {
+  const resolvedViewExtraSchema =
+    options.customViewExtraSchema?.(viewExtraSchema) ?? viewExtraSchema;
+
+  return z.object({
+    viewsComponent: createViewsComponentSchema(resolvedViewExtraSchema),
+  });
+};
