2d30ad5072a1dd02bd8568ced4b10c27c61ee747
Wojcik <dwojcik@luxoft.com>
2026-01-26 13:43:55 +0100

    wip

--- sol.tsx
+++ sol.tsx
@@ -1,14 +1,10 @@
-type UnwrapZodDefault<T> = T extends z.ZodDefault<infer U> ? U : T;
-
-type BaseViewExtraSchema = UnwrapZodDefault<typeof viewExtraSchema>;
-
 type ResolveViewExtraSchema<T> =
-  T extends { customViewExtraSchema: (base: BaseViewExtraSchema) => infer R }
+  T extends { customViewExtraSchema: (base: typeof viewExtraSchema) => infer R }
     ? R
     : typeof viewExtraSchema;
 
 export const createContentSchema = <
-  T extends Pick<CreateStorageModelOptions<z.ZodObject<{}>>, 'customViewExtraSchema'>
+  T extends Pick<CreateStorageModelOptions<z.ZodTypeAny>, 'customViewExtraSchema'>
 >({
   customViewExtraSchema,
 }: T) =>
@@ -17,7 +13,7 @@ export const createContentSchema = <
     settings: z.object({}),
     viewsComponent: createViewsComponentSchema(
       (customViewExtraSchema
-        ? customViewExtraSchema(viewExtraSchema._def.innerType)
-        : viewExtraSchema._def.innerType) as ResolveViewExtraSchema<T>
+        ? customViewExtraSchema(viewExtraSchema)
+        : viewExtraSchema) as ResolveViewExtraSchema<T>
     ),
   });
